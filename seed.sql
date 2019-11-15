0443fe818a21c151d96fb21b841c6b0eb9ed5e67
halseyjt <halseyjt@yahoo.com>
2019-11-15 12:11:31 -0600

    database and table created

---
+++ seed.sql
@@ -0,0 +1,10 @@
+TRUNCATE donuts;
+
+INSERT INTO donuts(filling, stale) VALUES ('blueberry', 0);
+INSERT INTO donuts(filling, stale) VALUES ('cream', 0);
+INSERT INTO donuts(filling, stale) VALUES ('strawberry', 0);
+INSERT INTO donuts(filling, stale) VALUES ('banana cream', 0);
+INSERT INTO donuts(filling, stale) VALUES ('nutella', 0);
+INSERT INTO donuts(filling, stale) VALUES ('toothpaste', 1);
+INSERT INTO donuts(filling, stale) VALUES ('donuts', 1);
+INSERT INTO donuts(filling, stale) VALUES ('chililime', 1);
